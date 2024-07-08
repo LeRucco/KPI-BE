8d3a291b9335c86bcc945852450dbafb8efec6bd
DRucci-88 <d.rucci.2001@gmail.com>
2024-07-09 04:16:19 +0700

    test 403

--- app/Http/Controllers/AttendancePermitController.php
+++ app/Http/Controllers/AttendancePermitController.php
@@ -31,6 +31,8 @@ class AttendancePermitController extends Controller
 
     public function detailDate(AttendancePermitDetailDateRequest $req)
     {   
+        
+        Gate::authorize('viewAny', [Attendance::class]);
         // TODO Policy
         $date = $req->date->format('Y-m-d');        // Selected Date
         /** @var \App\Models\User */
@@ -104,7 +106,6 @@ class AttendancePermitController extends Controller
 
     public function totalEmp(AttendancePermitTotalEmpRequest $req)
     {
-        Gate::authorize('viewAny', [Attendance::class]);
         // TODO Policy
         $selectedMonthYear = $req->date->format('Y-m'); // yyyy-MM
         $fromDate = $req->date->format('Y-m') . '-01';    // First Date of the Month
