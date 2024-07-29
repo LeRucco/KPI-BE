437847a92568930ea49180d16ff1a511bc2b5a74
DRucci-88 <d.rucci.2001@gmail.com>
2024-07-29 17:32:08 +0700

    Permit Check bug fix

--- app/Http/Controllers/PermitController.php
+++ app/Http/Controllers/PermitController.php
@@ -65,15 +65,16 @@ class PermitController extends Controller implements ApiBasicReadInterfaces
         Gate::authorize('check', [Permit::class]);
 
         $date = $req->date->format('Y-m-d');
-        $type = $req->type;
+        $type = $req->type == null ? null : $req->type->value;
         $status = $req->status == null ? null : $req->status->value;
         $userId = $req->userId;
 
         $result = DB::table('permits')
             ->join('users', 'permits.user_id', '=', 'users.id')
-            ->where(function (Builder $query) use ($date) {
-                $query->whereDate('permits.date', '=', $date);
-            })
+            ->whereDate('permits.date', '=', $date)
+            // ->when(function (Builder $query) use ($date) {
+            //     $query->whereDate('permits.date', '=', $date);
+            // })
             ->when($type, function (Builder $query, int $type) {
                 $query->where('permits.type', '=', $type);
             })
